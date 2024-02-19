d0865bf1c1ceec571658a880fcd0e9b145402f31
Indiamara Enes <indiamaraenes@gmail.com>
2024-02-19 14:17:23 -0300

    :tada:

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+<div class="container">
+    <h1>Editar Usuário</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('users.update', $user->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Nome:</label>
+            <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}">
+        </div>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}">
+        </div>
+        <button type="submit" class="btn btn-primary">Atualizar</button>
+    </form>
+</div>
+@endsection
